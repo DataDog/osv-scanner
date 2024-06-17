cf992664d3253146a5ece6b82112cf819f03173b
Victor Granados <victor.granadosharinero@datadoghq.com>
2024-06-17 14:52:08 +0200

    🐛 Fix wrongly reported locations (resolutions and name conflict)

--- pkg/lockfile/match-package-json.go
+++ pkg/lockfile/match-package-json.go
@@ -1,16 +1,23 @@
 package lockfile
 
 import (
-	"fmt"
 	"io"
 	"strings"
 
 	"github.com/google/osv-scanner/internal/cachedregexp"
+
 	"github.com/google/osv-scanner/internal/utility/fileposition"
 	"github.com/google/osv-scanner/pkg/models"
 )
 
 type PackageJSONMatcher struct{}
+
+const (
+	namePrefix    = "\""
+	nameSuffix    = "\":"
+	versionPrefix = ":\\s*\""
+	versionSuffix = "\",?"
+)
 
 func (m PackageJSONMatcher) GetSourceFile(lockfile DepFile) (DepFile, error) {
 	return lockfile.Open("package.json")
@@ -26,31 +33,31 @@ func (m PackageJSONMatcher) Match(sourcefile DepFile, packages []PackageDetails)
 	for index, line := range lines {
 		lineNumber := index + 1
 		for key, pkg := range packages {
-			if strings.Contains(line, pkg.Name) {
-				// TODO: what to do if version is not in the same line as the name?
+			// TODO: what to do if version is not in the same line as the name?
+			block := []string{line}
+			nameRegexp := cachedregexp.QuoteMeta(pkg.Name) + "(@.*)?"
+			nameLocation := fileposition.ExtractDelimitedRegexpPositionInBlock(block, nameRegexp, lineNumber, namePrefix, nameSuffix)
+			if nameLocation != nil {
 				for _, targetVersion := range pkg.TargetVersions {
-					if strings.Contains(line, targetVersion) {
+					var versionLocation *models.FilePosition
+					if targetVersion == pkg.Version {
+						versionLocation = fileposition.ExtractDelimitedRegexpPositionInBlock(block, targetVersion, lineNumber, versionPrefix, versionSuffix)
+					} else {
+						versionRegexp := ".*" + cachedregexp.QuoteMeta(targetVersion) + ".*"
+						versionLocation = fileposition.ExtractDelimitedRegexpPositionInBlock(block, versionRegexp, lineNumber, versionPrefix, versionSuffix)
+					}
+					if versionLocation != nil {
 						startColumn := fileposition.GetFirstNonEmptyCharacterIndexInLine(line)
 						endColumn := fileposition.GetLastNonEmptyCharacterIndexInLine(strings.TrimSuffix(line, ","))
-
 						packages[key].BlockLocation = models.FilePosition{
 							Line:     models.Position{Start: lineNumber, End: lineNumber},
 							Column:   models.Position{Start: startColumn, End: endColumn},
 							Filename: sourcefile.Path(),
 						}
-
-						nameLocation := fileposition.ExtractDelimitedStringPositionInBlock([]string{line}, pkg.Name, lineNumber, "\"", "\"")
-						if nameLocation != nil {
-							nameLocation.Filename = sourcefile.Path()
-							packages[key].NameLocation = nameLocation
-						}
-
-						versionRegexp := fmt.Sprintf(".*%s.*", cachedregexp.QuoteMeta(targetVersion))
-						versionLocation := fileposition.ExtractDelimitedRegexpPositionInBlock([]string{line}, versionRegexp, lineNumber, ":\\s*\"", "\",?")
-						if versionLocation != nil {
-							versionLocation.Filename = sourcefile.Path()
-							packages[key].VersionLocation = versionLocation
-						}
+						nameLocation.Filename = sourcefile.Path()
+						packages[key].NameLocation = nameLocation
+						versionLocation.Filename = sourcefile.Path()
+						packages[key].VersionLocation = versionLocation
 					}
 				}
 			}
