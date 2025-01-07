a527fa22dba5fbf20607f6e62f7160463d5364ca
Amaan Qureshi <amaan.qureshi@datadoghq.com>
2025-01-07 15:37:39 -0500

    feat: support npm workspaces for resolving dependency locations

--- pkg/lockfile/match-package-json.go
+++ pkg/lockfile/match-package-json.go
@@ -1,16 +1,26 @@
 package lockfile
 
 import (
+	"encoding/json"
 	"io"
+	"path/filepath"
 	"strings"
 
+	"github.com/bmatcuk/doublestar"
 	"github.com/google/osv-scanner/internal/cachedregexp"
 
 	"github.com/google/osv-scanner/internal/utility/fileposition"
 	"github.com/google/osv-scanner/pkg/models"
 )
 
-type PackageJSONMatcher struct{}
+type PackageJSONMatcher struct {
+	// Used to store the patterns for workspaces in a given root package.json
+	WorkspacePatterns []string
+}
+
+type WorkspacePackageJSON struct {
+	Workspaces []string `json:"workspaces"`
+}
 
 const (
 	namePrefix       = "\""
@@ -54,12 +64,75 @@ func updatePackageLocations(pkg *PackageDetails, nameLocation *models.FilePositi
 	pkg.IsDirect = true
 }
 
+func globWorkspacePackageJsons(workspacePatterns []string, basePath string) []string {
+	var results []string
+
+	for _, pattern := range workspacePatterns {
+		// Convert npm workspace pattern to package.json file pattern
+		// e.g. "packages/*" -> "packages/*/package.json"
+		searchPattern := filepath.Join(filepath.Dir(basePath), pattern, "package.json")
+
+		matches, err := doublestar.Glob(searchPattern)
+		if err != nil {
+			continue
+		}
+
+		baseDir := filepath.Dir(basePath)
+		for _, match := range matches {
+			relPath, err := filepath.Rel(baseDir, match)
+			if err != nil {
+				continue
+			}
+			results = append(results, relPath)
+		}
+	}
+
+	return results
+}
+
 func (m PackageJSONMatcher) Match(sourcefile DepFile, packages []PackageDetails) error {
+	var wpj struct {
+		Workspaces []string `json:"workspaces"`
+	}
+
 	content, err := io.ReadAll(sourcefile)
 	if err != nil {
 		return err
 	}
 
+	if err := json.Unmarshal(content, &wpj); err != nil {
+		m.matchFile(sourcefile, packages, content)
+		return nil
+	}
+
+	// Match in root package.json
+	m.matchFile(sourcefile, packages, content)
+
+	// For workspaces, find and match in workspace package.json files
+	if len(wpj.Workspaces) > 0 {
+		matches := globWorkspacePackageJsons(wpj.Workspaces, sourcefile.Path())
+
+		for _, match := range matches {
+			workspacePkg, err := sourcefile.Open(match)
+			if err != nil {
+				continue
+			}
+			defer workspacePkg.Close()
+
+			workspaceContent, err := io.ReadAll(workspacePkg)
+			if err != nil {
+				continue
+			}
+
+			// Match dependencies in workspace package.json
+			m.matchFile(workspacePkg, packages, workspaceContent)
+		}
+	}
+
+	return nil
+}
+
+func (m PackageJSONMatcher) matchFile(file DepFile, packages []PackageDetails, content []byte) {
 	lines := fileposition.BytesToLines(content)
 	for index, line := range lines {
 		lineNumber := index + 1
@@ -67,17 +140,14 @@ func (m PackageJSONMatcher) Match(sourcefile DepFile, packages []PackageDetails)
 			nameLocation := tryGetNameLocation(pkg.Name, line, lineNumber)
 			if nameLocation != nil {
 				for _, targetVersion := range pkg.TargetVersions {
-					// TODO: what to do if version is not in the same line as the name?
 					versionLocation := tryGetVersionLocation(targetVersion, line, lineNumber)
 					if versionLocation != nil {
-						updatePackageLocations(&packages[key], nameLocation, versionLocation, line, lineNumber, sourcefile.Path())
+						updatePackageLocations(&packages[key], nameLocation, versionLocation, line, lineNumber, file.Path())
 					}
 				}
 			}
 		}
 	}
-
-	return nil
 }
 
 var _ Matcher = PackageJSONMatcher{}
