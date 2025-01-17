65730a3cd57b29cfdc7bfb7b497429895551d7d2
Marc Wieser <marc.wieser@datadoghq.com>
2025-01-17 11:11:23 +0100

    Bring back windows replacements in test utility

--- internal/testutility/snapshot.go
+++ internal/testutility/snapshot.go
@@ -6,11 +6,29 @@ import (
 	"github.com/gkampitakis/go-snaps/snaps"
 )
 
-type Snapshot struct{}
+type Snapshot struct {
+	windowsReplacements map[string]string
+}
 
 // NewSnapshot creates a snapshot that can be passed around within tests
 func NewSnapshot() Snapshot {
-	return Snapshot{}
+	return Snapshot{windowsReplacements: map[string]string{}}
+}
+
+// WithWindowsReplacements adds a map of strings with values that they should be
+// replaced within before comparing the snapshot when running on Windows
+func (s Snapshot) WithWindowsReplacements(replacements map[string]string) Snapshot {
+	s.windowsReplacements = replacements
+
+	return s
+}
+
+// WithCRLFReplacement adds a Windows replacement for "\r\n" to "\n".
+// This should be called after WithWindowsReplacements if used together.
+func (s Snapshot) WithCRLFReplacement() Snapshot {
+	s.windowsReplacements["\r\n"] = "\n"
+
+	return s
 }
 
 // CleanSnapshots ensures that snapshots are relevant and sorted for consistency
